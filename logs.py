185bd05b9ae9bf16038d9e512b08117d2bf9212e
LightOfMars <112195965+LightOfMars@users.noreply.github.com>
2022-09-14 14:45:04 -0500

    finished
    poorly commented ;)

--- logs.py
+++ logs.py
@@ -1,13 +1,23 @@
 # logs.py
-#
+#must be (-10,7]
 # Outputs the value of g(x) = ln(100-x^2) + sqrt(84 - 5x - x^2) at the input x-value.
 
 import math
 
+#functions
 def g(x):
     return math.log(100 - x**2) + math.sqrt(84 - 5*x - x**2)
 
+#inputs + convert to float
 x = input("Input the value for x: ")
 x = float(x)
 
-print("g(" + str(x) + ") = " + str(g(x)))
+#checking correct value
+if x <= -10 or x > 7 :
+    print("Input not valid, please try again.")
+    exit()
+elif x:
+    print("g(" + str(x) + ") = " + str(g(x)))
+
+
+#print("g(" + str(x) + ") = " + str(g(x)))
